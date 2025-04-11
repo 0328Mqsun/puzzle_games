60cc324625ed7f5c09325ed25e7b1f08260a8d65
Mqsun <shawn.sun0328@gmail.com>
2025-04-12 00:47:42 +0800

    Initial commit

---
+++ src/main/java/com/example/puzzle_games/PuzzleGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.puzzle_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleGamesApplication.class, args);
+	}
+
+}
